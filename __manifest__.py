189317e460515497a8ceb2c86cc35bed3c9746e0
Rafly Herdianto <raflyherdianto25@gmail.com>
2023-02-20 14:19:14 +0700

    menambahkan addons Odoo

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "New Addons",
+    "version": "1.0",
+    "author": "Mochammad Rafly Herdianto",
+    "website": "https://www.detik.com/",
+    "summary": "Ini adalah addons baru",
+    "description": """
+      Latihan membuat addons baru di odoo 1
+    """,
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "demo": [],
+    "test": [],
+    "image": [],
+    "qweb": [],
+    "css": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
